e71b8fec696372194d9ab0b9743bcc94ccb35e0a
Waheed-Labib <labibwahid28@gmail.com>
2025-02-11 18:05:05 +0600

    added address to contactUs section

--- lib/fakeData.tsx
+++ lib/fakeData.tsx
@@ -173,3 +173,4 @@ export const fakeRegions = [
 
 export const hotline = '01788458189'
 export const whatsAppNumber = '8801788458189'
+export const address = '4th floor, House No. 20, Central Road, Maijdee Court, Noakhali'
--- ui/home/contact/contactUs.tsx
+++ ui/home/contact/contactUs.tsx
@@ -1,7 +1,8 @@
 import { BsFillTelephoneFill } from "react-icons/bs";
 import Header from "../../header";
-import { hotline } from "../../../lib/fakeData";
+import { address, hotline } from "../../../lib/fakeData";
 import CopyNumberBtn from "./copyNumberBtn";
+import { ImOffice } from "react-icons/im";
 
 export default function ContactUs() {
 
@@ -13,9 +14,9 @@ export default function ContactUs() {
                 underlineColor="primary"
             ></Header>
 
-            <div className="card bg-primary-content/95 text-base-100 w-full">
+            <div className="card bg-primary-content/95 text-base-100 w-full py-4">
                 <div className="card-body w-full">
-                    <div className="w-full flex justify-center items-center gap-8 py-4">
+                    <div className="w-full flex justify-center items-center gap-4 md:gap-8 py-8 flex-col md:flex-row text-center">
                         <div className="flex items-center gap-2 text-accent">
                             <div className="text-2xl text-accent">
                                 <BsFillTelephoneFill></BsFillTelephoneFill>
@@ -23,17 +24,26 @@ export default function ContactUs() {
                             <p className="text-base-200">Hotline :</p>
                         </div>
 
-                        <div>
+                        <div className="flex items-center gap-2 md:gap-4 flex-col md:flex-row">
                             <p className="text-lg text-base-200">{hotline}</p>
+                            <CopyNumberBtn></CopyNumberBtn>
                         </div>
 
-                        <CopyNumberBtn></CopyNumberBtn>
                     </div>
 
                     <hr className="border-accent" />
 
-                    <div className="w-full flex justify-center">
-                        address
+                    <div className="w-full flex justify-center items-center gap-4 md:gap-8 py-8 flex-col md:flex-row text-center">
+                        <div className="flex items-center gap-2 md:gap-4 text-accent">
+                            <div className="text-2xl text-accent">
+                                <ImOffice />
+                            </div>
+                            <p className="text-base-200">Address :</p>
+                        </div>
+
+                        <div>
+                            <p className="text-sm text-base-200">{address}</p>
+                        </div>
                     </div>
                 </div>
             </div>
