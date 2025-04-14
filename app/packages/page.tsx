53130e26abb6ba46d737d5f20a3f6f8572b62b3f
Waheed-Labib <labibwahid28@gmail.com>
2025-04-14 11:49:18 +0600

    updated breadcrumbs design

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -6,15 +6,16 @@ export default function Page() {
     return (
         <div className="">
             <div
-                className="breadcrumbs text-neutral bg-cover bg-center h-48 p-0"
+                className="bg-cover bg-center h-48 p-0"
                 style={{ backgroundImage: "url('/assets/breadcrumbsBG.png')" }}
             >
-                <ul className="bg-gradient-to-r from-slate-50/50 via-slate-50 to-slate-50/50 w-full h-full justify-center p-8">
-                    <li><Link href="/" className="">Home</Link></li>
-                    <li className="">Packages</li>
-                </ul>
+                <div className="breadcrumbs flex justify-center items-center w-full h-full bg-gradient-to-tr from-neutral/25 to-neutral">
+                    <ul className="text-sm bg-neutral text-slate-50 py-2 px-4 shadow-sm shadow-slate-50">
+                        <li><Link href="/" className="">Home</Link></li>
+                        <li className="">Packages</li>
+                    </ul>
+                </div>
             </div>
-
             <div>
                 <Packages></Packages>
             </div>
