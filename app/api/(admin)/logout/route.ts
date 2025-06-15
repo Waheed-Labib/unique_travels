cb738395eafab0053771f1ce9eb303a6eaafea7b
Waheed-Labib <labibwahid28@gmail.com>
2025-06-15 11:28:41 +0600

    created api/logout

---
+++ app/api/(admin)/logout/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+
+export async function POST() {
+
+    const cookieStore = await cookies();
+
+    cookieStore.set('token', '', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
+        maxAge: 0,
+    })
+
+    return NextResponse.json({ message: 'Logged out successfully' }, { status: 200 })
+}
