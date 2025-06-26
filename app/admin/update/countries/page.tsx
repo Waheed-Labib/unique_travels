1cf45297144587d0f91cd185b790133c4abd3e7d
Waheed-Labib <labibwahid28@gmail.com>
2025-06-26 20:08:04 +0600

    updated /admin/update/countries

--- app/admin/update/countries/page.tsx
+++ app/admin/update/countries/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import { country } from '../../../../lib/types';
+import { FaPlus } from "react-icons/fa";
+import { cn } from '../../../../lib/utils';
 
 const Page = () => {
+
+    const [originalCountries, setOriginalCountries] = useState<country[]>([])
+
+    const [dataLoading, setDataLoading] = useState(true);
+
+    useEffect(() => {
+
+        const getCountries = async () => {
+            try {
+                const res = await fetch('/api/countries');
+                const data = await res.json();
+                setOriginalCountries(data.data);
+            } catch (error) {
+                console.error(error)
+            } finally {
+                setDataLoading(false)
+            }
+        }
+
+        getCountries();
+    }, [])
+
+    if (dataLoading) {
+        return <div className='text-center'>
+            <p>Loading Countries ...</p>
+        </div>
+    }
+
     return (
-        <div className='text-neutral'>
-            update countries
+        <div className='text-neutral flex flex-col items-center gap-4'>
+            <button className='btn btn-primary glass bg-primary'>
+                <FaPlus></FaPlus>
+                <p className=''>Add New Country</p>
+            </button>
+
+            <div>
+                {
+                    originalCountries.map(country => <p key={country._id}>{country.name}</p>)
+                }
+            </div>
         </div>
     );
 };
