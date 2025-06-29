2d5d95d2c1efaf7ddc24cca5d27ad2c0e9ef22d6
Waheed-Labib <labibwahid28@gmail.com>
2025-06-29 17:51:53 +0600

    updated delete-country page

--- app/admin/update/countries/delete/[countryName]/page.tsx
+++ app/admin/update/countries/delete/[countryName]/page.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import DashboardHeading from '../../../../dashboardHeading';
+'use client'
 
-const Page = async ({
+import React, { useEffect, useState } from 'react';
+import DashboardHeading from '../../../../dashboardHeading';
+import { pkg } from '../../../../../../lib/types';
+
+const Page = ({
     params,
 }: {
     params: Promise<{ countryName: string }>;
 }) => {
 
-    const { countryName } = await params;
+    const { countryName } = React.use(params);
+
+    const [packages, setPackages] = useState<pkg[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const getPackages = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch(`http://localhost:3000/api/packages?country=${countryName}`);
+                const data = await res.json();
+                setPackages(data.data);
+            } catch (error) {
+                console.error(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        getPackages();
+    }, [countryName])
 
     return (
         <div className='flex flex-col items-center gap-8'>
             <DashboardHeading>
                 Delete {countryName}?
             </DashboardHeading>
+
+            {
+                packages.length > 1 &&
+                <p className='text-sm italic font-medium'>{countryName} is associated with <span className='text-accent font-semibold'>{packages.length} packages</span>.</p>
+            }
+
+            {
+                packages.length === 1 &&
+                <p className='text-sm italic font-medium'>{countryName} is associated with <span className='text-accent font-semibold'>1 package</span></p>
+            }
+
+            {
+                loading ?
+                    <p>Loading ... </p>
+                    :
+                    <div>
+                        <button>Delete</button>
+                        <button>Cancel</button>
+                    </div>
+            }
         </div>
     );
 };
